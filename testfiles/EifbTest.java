99790452ae045497d09a8cbbcca3f464ecaa2eb6
Mark Lundager <Mark_06_20@outllok.com>
2023-11-23 16:53:51 +0100

    Long operations to short for varuse and literal

---
+++ testfiles/EifbTest.java
@@ -0,0 +1,24 @@
+public class EifbTest {       
+    int x = 4;
+
+    public static void main(String[] args) {
+        eifb(2);
+    }
+
+    
+    public boolean eifb(int y){
+        if(x>/*Comment in middle of expression*/y){
+        }//comment here as well
+        else{
+            return/*this is a crazy comment location*/ false;
+        }
+    }
+
+    public boolean eifb2(int y){
+        if(!(x > y)){
+            return false;
+        }
+    }
+
+}
+
